524d8dae3ccf95ac5119c3270b0ce373977ebea1
grapgrap <fussaep@gmail.com>
2023-05-13 00:43:00 +0900

    chore(eslint-config-nether): Add eslint config package

---
+++ packages/eslint-config-nether/configs/base.cjs
@@ -0,0 +1,16 @@
+require("../patch/modern-module-resolution.cjs");
+
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+};
