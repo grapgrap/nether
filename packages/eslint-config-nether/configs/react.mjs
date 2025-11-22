fbbcf3a7bbd9cfe549fcf04462ecc5add6152409
grapgrap <fussaep@gmail.com>
2025-11-22 20:39:14 +0900

    chore: Add react-refresh plugin on eslint

--- packages/eslint-config-nether/configs/react.mjs
+++ packages/eslint-config-nether/configs/react.mjs
@@ -1,6 +1,7 @@
 import react from "eslint-plugin-react";
 import hooks from "eslint-plugin-react-hooks";
+import refresh from "eslint-plugin-react-refresh";
 import { defineConfig } from "eslint/config";
 import base from "./base.mjs";
 
-export default defineConfig(base, react.configs.flat.recommended, hooks.configs.flat.recommended);
+export default defineConfig(base, react.configs.flat.recommended, hooks.configs.flat.recommended, refresh.configs.recommended);
